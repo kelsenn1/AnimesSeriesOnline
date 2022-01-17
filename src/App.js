403abd742151ee43bc030e1e4dd3c00a853bfb83
Lucca Proença souza <luccaproenca@gmail.com>
2022-01-17 00:46:14 -0300

    f###ing paginas

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/home" element={<Home/>} component={Home} />
+          <Route path="/" element={<Home/>} component={Home} />
           <Route path="/teste" element={<Teste/>} component={Teste} />
         </Routes>
       </BrowserRouter>
